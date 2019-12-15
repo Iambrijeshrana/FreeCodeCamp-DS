5a933668374355ae9a9b957b06456136415f1813
Brijesh Rana <46286358+Iambrijeshrana@users.noreply.github.com>
2019-12-15 14:25:55 +0530

    Update HousingPrice.py
    Step1 and Step2 completed.

--- With Python/LinearRegression/HousingPrice.py
+++ With Python/LinearRegression/HousingPrice.py
@@ -7,3 +7,73 @@ Created on Sun Dec 15 13:24:19 2019
 
 "In this we will try to predict the price of House based on one predictor"
 
+'''-------------------- Data set description --------------------
+CRIM: Per capita crime rate by town
+ZN: Proportion of residential land zoned for lots over 25,000 sq. ft
+INDUS: Proportion of non-retail business acres per town
+CHAS: Charles River dummy variable (= 1 if tract bounds river; 0 otherwise)
+NOX: Nitric oxide concentration (parts per 10 million)
+RM: Average number of rooms per dwelling
+AGE: Proportion of owner-occupied units built prior to 1940
+DIS: Weighted distances to five Boston employment centers
+RAD: Index of accessibility to radial highways
+TAX: Full-value property tax rate per $10,000
+PTRATIO: Pupil-teacher ratio by town
+B: 1000(Bk — 0.63)², where Bk is the proportion of [people of African American descent] by town
+LSTAT: Percentage of lower status of the population
+MEDV: Median value of owner-occupied homes in $1000s
+'''
+
+import pandas as pd # Import pandas to import and analyze the data
+import numpy as np # Import numpy to perform mathematical funcations on data
+
+import plotly
+%matplotlib inline
+import plotly.plotly as py
+import matplotlib.pyplot as plt # Import pyplot
+import seaborn as sns
+from matplotlib import style
+from scipy import stats
+from sklearn.model_selection import train_test_split # Import train_test_split function
+from sklearn.linear_model import LinearRegression # Import Linear Regression
+from sklearn import metrics
+from sklearn.metrics import r2_score, mean_squared_error, adjusted_rand_score
+
+"************************** Step 1. Collect the data *****************************"
+
+# Import the Housing Price dataset
+housingData = pd.read_csv("D:/Personal/Dataset/housingdata.csv")
+
+# Rename the columns (Give the name to columns)
+housing_colnames = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV']
+housingData.columns = housing_colnames
+
+housingData.columns
+
+"************************** Step 2. Analyze the data *****************************"
+
+# Check the shape of data
+housingData.shape
+
+# To check the relationship (Correlation) between target variable (MEDV) and dependent variable 
+
+# To see the correlation in tabular format
+housingData.corr()
+
+# To plot the correlation of features with target variable using heatmap of seaborn
+fig = plt.subplots(figsize = (10,10))
+sns.set(font_scale=1.5)
+sns.heatmap(housingData.corr(),square = True,cbar=True,annot=True,annot_kws={'size': 10})
+plt.show()
+
+''' well correlation between RM and MEDV is very high (.70), so we are going to build the model based on RM'''
+
+# Plot Simple histogram to see the distribution of MEDV data in the dataset 
+plt.hist(housingData['MEDV'])
+
+# Plot histogram with curve line to see the distribution of MEDV data in the dataset
+sns.distplot(housingData['MEDV'], hist=True, kde=True, 
+             bins=int(180/5), color = 'darkblue', 
+             hist_kws={'edgecolor':'black'},
+             kde_kws={'linewidth': 4})
+
