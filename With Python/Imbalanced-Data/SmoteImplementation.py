37b4afefde5b11302542ade0beec1aa349e9d74b
Brijesh Rana <46286358+Iambrijeshrana@users.noreply.github.com>
2019-12-24 14:40:10 +0530

    SMOTE implementation code added.

--- With Python/Imbalanced-Data/SmoteImplementation.py
+++ With Python/Imbalanced-Data/SmoteImplementation.py
@@ -65,6 +65,28 @@ So, it proves that this is not the best model.Now, we will apply different imbal
 and see their accuracy and recall results.
 '''
 
+print("Before OverSampling, counts of label '1': {}".format(sum(y_train == 1))) 
+print("Before OverSampling, counts of label '0': {} \n".format(sum(y_train == 0))) 
+
+# import SMOTE module from imblearn library 
+# pip install imblearn (if you don't have imblearn in your system) 
+from imblearn.over_sampling import SMOTE 
+sm = SMOTE(random_state = 2) 
+X_train_res, y_train_res = sm.fit_sample(X_train, y_train.ravel()) 
+
+print('After OverSampling, the shape of train_X: {}'.format(X_train_res.shape)) 
+print('After OverSampling, the shape of train_y: {} \n'.format(y_train_res.shape)) 
+
+print("After OverSampling, counts of label '1': {}".format(sum(y_train_res == 1))) 
+print("After OverSampling, counts of label '0': {}".format(sum(y_train_res == 0))) 
+
+'''
+Look! that SMOTE Algorithm has oversampled the minority instances and made it equal to majority class. Both 
+categories have equal amount of records. More specifically, the minority class has been increased to the total 
+number of majority class. Now see the accuracy and recall results after applying SMOTE algorithm (Oversampling).
+'''
+
+
 
 
 
