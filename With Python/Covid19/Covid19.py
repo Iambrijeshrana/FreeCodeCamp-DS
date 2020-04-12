6fe69b985e8c3f05052a78a60ddb228ce0ccc86d
Brijesh Rana <46286358+Iambrijeshrana@users.noreply.github.com>
2020-04-12 09:30:39 +0530

    Update Covid19.py
    Code to draw plots added.

--- With Python/Covid19/Covid19.py
+++ With Python/Covid19/Covid19.py
@@ -10,6 +10,7 @@ import numpy as np
 import seaborn as sns
 from matplotlib import pyplot
 import mylib
+import matplotlib.pylab as plt
 
 confirmcase = pd.read_csv("D:/Personal/Dataset/Covid19/main/time_series_covid19_confirmed_global_narrow.csv")
 confirmcase 
@@ -26,19 +27,55 @@ confirmcase.columns
 indiaconfirmcase = confirmcase.loc[confirmcase['Country/Region']=='India']
 
 indiaconfirmcase.head()
-sns.barplot(x='Date', y='Value', data = indiaconfirmcase.head(),orient="v")
 
 indiandatawithoutzero = indiaconfirmcase.loc[indiaconfirmcase['Value']>0]
-
-
-indiandatawithoutzero.shape
-sns.set(rc={'figure.figsize':(10,15)})
-sns.countplot(y='Date', x='Value', data = indiandatawithoutzero,orient="h")
 
 grouped = confirmcase.groupby('Country/Region')
 
 print(grouped['Value'].agg(np.sum))
 
 a=grouped['Value'].agg(np.sum)
-
+    
+indiandatawithoutzero.shape
+sns.set(rc={'figure.figsize':(12,12)})
+g=sns.barplot(y='Date', x='Value', data = indiandatawithoutzero,orient="h")
+for index, row in indiandatawithoutzero.iterrows():
+    g.text(row.name,row.Value, round(row.Value,2), color='black')
+    
 sns.lineplot(x='Date', y='Value', data = indiaconfirmcase)
+
+sns.distplot(indiandatawithoutzero.Value)
+
+# sort indiandatawithoutzero by Date column
+indiandatawithoutzero = indiandatawithoutzero.sort_values(['Value']).reset_index(drop=True)
+
+
+plt.figure(figsize=(16,10))
+# plot barh chart with index as x values
+ax = sns.barplot(indiandatawithoutzero.index, indiandatawithoutzero.Value, color='red')
+ax.get_yaxis().set_major_formatter(plt.FuncFormatter(lambda x, loc: "{:,}".format(int(x))))
+ax.set(xlabel="Date", ylabel='Value')
+# add proper Dim values as x labels
+ax.set_xticklabels(indiandatawithoutzero.Date)
+for item in ax.get_xticklabels(): item.set_rotation(90)
+for i, v in enumerate(indiandatawithoutzero["Value"].iteritems()):        
+    ax.text(i ,v[1], "{:,}".format(v[1]), color='m', va ='bottom', rotation=90)
+plt.tight_layout()
+plt.show()
+
+# ---------------------------------------------------
+
+
+
+plt.figure(figsize=(16,10))
+# plot barh chart with index as x values
+ax = sns.barplot(indiandatawithoutzero.index, indiandatawithoutzero.Value, color='red')
+ax.get_yaxis().set_major_formatter(plt.FuncFormatter(lambda x, loc: "{:,}".format(int(x))))
+ax.set(xlabel="Date", ylabel='Value')
+# add proper Dim values as x labels
+ax.set_xticklabels(indiandatawithoutzero.Date)
+for item in ax.get_xticklabels(): item.set_rotation(90)
+for i, v in enumerate(indiandatawithoutzero["Value"].iteritems()):        
+    ax.text(i ,v[1], "{:,}".format(v[1]), color='m', va ='bottom', rotation=90)
+plt.tight_layout()
+plt.show()
