1e291689b4d5c34149711010c919dc2c82ea4798
Brijesh Rana <iambrijeshrana@gmail.com>
2021-11-28 22:18:55 +0530

    Update untitled2.py

--- With Python/GroupedTimeSeriesForecasting/untitled2.py
+++ With Python/GroupedTimeSeriesForecasting/untitled2.py
@@ -11,7 +11,7 @@ t=turtle.Turtle()
 screen=turtle.Screen()
 screen.bgcolor('black')
 t.speed(25)
-for i in range(150):
+for i in range(250):
     t.color(col[i%6])
     t.forward(i*1.5)
     t.left(59)
